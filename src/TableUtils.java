9b13ee26f3f6c966dac340ac5bffffd4c5792252
aisw-123 <38095249+aisw-123@users.noreply.github.com>
2024-11-24 01:40:07 -0600

    Update TableUtils.java
    Updated function name splashscreen to displayWelcomeScreen() name to match the main class

--- src/TableUtils.java
+++ src/TableUtils.java
@@ -1,6 +1,6 @@
 public class TableUtils {
 
-	public static void splashScreen() {
+	public static void displayWelcomeScreen()() {
 
 		// Print out the entry screen
 		System.out.println(printSeparator("-",80));
